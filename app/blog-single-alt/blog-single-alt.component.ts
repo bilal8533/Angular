eb74a198485e141767e14d0d95f8b0eea869925f
Mohd Bilal <69635539+bilal8533@users.noreply.github.com>
2022-07-15 11:40:25 +0530

    Add files via upload

---
+++ app/blog-single-alt/blog-single-alt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-single-alt',
+  templateUrl: './blog-single-alt.component.html',
+  styleUrls: ['./blog-single-alt.component.css']
+})
+export class BlogSingleAltComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ app/minimal/minimal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-minimal',
+  templateUrl: './minimal.component.html',
+  styleUrls: ['./minimal.component.css']
+})
+export class MinimalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
